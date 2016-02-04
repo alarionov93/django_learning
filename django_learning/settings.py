b36ce37f0ae71fa7b558e452ac1b473ce439a90f
alarionov93 <alarionov93@yandex.ru>
2016-02-04 06:37:53 +0500

    created userprofile app to extend user model

--- django_learning/settings.py
+++ django_learning/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'userprofile',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -109,3 +110,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_PROFILE_MODULE = 'userprofile.UserProfile'
---
+++ userprofile/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from userprofile.models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = UserProfile
+        fields = ('likes_cheese', )
+
